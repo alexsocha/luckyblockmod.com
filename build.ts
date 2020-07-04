1f572cfd01183a12c92001c2d7ef851ce22dfcb3
alexsocha <alex@alexsocha.com>
2020-07-04 14:18:36 +1000

    feat: add navbar and analytics templates

--- build.ts
+++ build.ts
@@ -5,27 +5,27 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
 
-const publicFolder = './public';
+const publicFolder = 'public';
 
 const globAsync = promisify(glob);
 
 const registerPartials = async () => {
-    const filePaths = await globAsync('src/partials/**/*.hbs');
+    const filePaths = await globAsync('src/partials/**/*.html');
     for await (const filePath of filePaths) {
         const contents = await fs.promises.readFile(filePath, 'utf-8');
-        Handlebars.registerPartial(path.basename(filePath, '.hbs'), contents);
+        Handlebars.registerPartial(path.basename(filePath, path.extname(filePath)), contents);
     }
 };
 
 const generatePages = async () => {
-    const filePaths = await globAsync('src/pages/**/*.{hbs,html}');
+    const filePaths = await globAsync('src/pages/**/*.html');
     for await (const filePath of filePaths) {
         const contents = await fs.promises.readFile(filePath, 'utf-8');
         const template = Handlebars.compile(contents);
 
         const publicPath = path.join(publicFolder, path.relative('src/pages', filePath));
         await fs.promises.mkdir(path.dirname(publicPath), { recursive: true });
-        await fs.promises.writeFile(publicPath, contents);
+        await fs.promises.writeFile(publicPath, template({}));
     }
 };
 
