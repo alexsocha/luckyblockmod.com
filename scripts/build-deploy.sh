0293798e2bc087f13f609abcc4e63155ea72e505
alexsocha <alex@alexsocha.com>
2020-07-31 12:41:42 +1000

    build: remove build stage from deploy script

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -1,20 +1,10 @@
 root_dir=$(dirname "$0")/..
 cd $root_dir
 
-docker-compose up build
+# make sure to first run 'docker-compose up build'
 
-rsync -aiR \
-    ./docs/dist \
-    ./client/dist \
-    ./server/dist \
-    ./server/nginx-conf \
-    ./server/dhparam \
-    ./server/package.json \
-    ./server/package.json \
-    ./server/package-lock.json \
-    ./package.json \
-    ./docker-compose.yaml \
-    ./Dockerfile \
-    root@luckyblockmod.com:~/luckyblock-website
+if [ -z "$(ls - server/dist)" ]; then
+   echo "Empty"
+fi
 
-ssh root@luckyblockmod.com "cd luckyblock-website && docker exec -t app npx pm2 reload app"
+
---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+root_dir=$(dirname "$0")/..
+cd $root_dir
+
+# make sure to first run 'docker-compose up build'
+
+if [ -z "$(ls server/dist)" ]; then exit 1; fi
+if [ -z "$(ls client/dist)" ]; then exit 1; fi
+if [ -z "$(ls docs/dist)" ]; then exit 1; fi
+
+rsync -aiR \
+    ./docs/dist \
+    ./client/dist \
+    ./server/dist \
+    ./server/nginx-conf \
+    ./server/dhparam \
+    ./server/package.json \
+    ./server/package.json \
+    ./server/package-lock.json \
+    ./package.json \
+    ./docker-compose.yaml \
+    ./Dockerfile \
+    root@luckyblockmod.com:~/luckyblock-website
+
+ssh root@luckyblockmod.com "cd luckyblock-website && docker exec -t app npx pm2 reload app"
