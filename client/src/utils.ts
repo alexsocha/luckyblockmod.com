1e8209f5fa044532318d6e13f3cc040397034db1
alexsocha <alex@alexsocha.com>
2021-01-03 13:38:19 +1100

    chore: remove console.log

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -2,7 +2,6 @@ const validateDownload = () => {
     const host = window.location.host;
     const referrer = document.referrer;
     const referrerHost = document.referrer.split('/')[2] || '';
-    console.log(referrerHost);
 
     // make sure that other sites don't link directly to download pages
     if (referrerHost !== host) {
