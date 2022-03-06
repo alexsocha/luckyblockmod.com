6d65fbd6ea7aafbbb48b7da357656791e5131bef
alexsocha <alex@alexsocha.com>
2022-03-06 15:42:17 +1100

    fix: nginx service name in ssl renew script

--- scripts/ssl_renew.sh
+++ scripts/ssl_renew.sh
@@ -2,5 +2,5 @@ COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd ~/luckyblock-website
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
